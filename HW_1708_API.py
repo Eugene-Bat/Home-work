e5b5df98b81e985e56ba9fd9cb57814aa391c903
Eugene-Bat <e.baturinetc@gmail.com>
2022-08-19 14:21:25 +0300

    api weather v2

--- HW_1708_API.py
+++ HW_1708_API.py
@@ -5,8 +5,11 @@ def weather(city_name):
     url = f'http://api.weatherapi.com/v1/current.json?key={api_key}={city_name}'
     x = requests.get(url)
     json = x.json()
-    return f'city/region: {json["location"]["name"]}\nlast update: {json["current"]["last_updated"]}\n' \
+
+    response = f'city/region: {json["location"]["name"]}\nlast update: {json["current"]["last_updated"]}\n' \
            f'Temp(c) = {json["current"]["temp_c"]}\nwind = {json["current"]["wind_kph"]}\n' \
            f'{json["current"]["condition"]["text"]}'
+
+    return response
 
 print(weather('london'))
